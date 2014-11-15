e7fac075b8d130a6475563450e00f5ff9d50231c
John Madison <johnmadison@NUNYA-BIZNAZZ.local>
2014-11-15 10:46:07 -0700

    Initial Commit

---
+++ roman numerals/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  roman numerals
+//
+//  Created by John Madison on 11/15/14.
+//  Copyright (c) 2014 cmu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
